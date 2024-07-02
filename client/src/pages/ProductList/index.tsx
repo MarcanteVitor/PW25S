db7002e975dd008c377e5aa5f3c3fa8fdea4c99a
Gustavo Guedes <gugaguedes11@gmail.com>
2024-07-01 21:44:41 -0300

    ajuste carrinho

--- client/src/pages/ProductList/index.tsx
+++ client/src/pages/ProductList/index.tsx
@@ -1,97 +1,101 @@
+import React, { useState, useEffect } from 'react';
 import '../../App.css';
 import { useNavigate } from "react-router-dom";
-import { Button } from '@chakra-ui/react'
-import { useEffect, useState } from 'react';
+import { Button, Modal } from 'react-bootstrap';
+import { BsCart2 } from "react-icons/bs";
 import { IProduct } from "@/commons/interfaces";
 import ProductService from "@/service/ProductService";
 import { CiSearch } from "react-icons/ci";
-import { BsCart2 } from "react-icons/bs";
-import AuthService from '@/service/AuthService';
 import 'bootstrap/dist/css/bootstrap.css';
-import ModalExemplo from "../cart/index";
-
 
 export function ProductList() {
   const [data, setData] = useState<IProduct[]>([]);
-  const [dataFiltred, setDataFiltred] = useState<IProduct[]>([]);
+  const [dataFiltered, setDataFiltered] = useState<IProduct[]>([]);
   const [apiError, setApiError] = useState("");
-  const [showCart, setShowCart] = useState(false);
+  const [showModal, setShowModal] = useState(false);
+  const [cartItems, setCartItems] = useState<{ produtoId: number, produtoNome: string, produtoValorTotal: number, quantidade: number }[]>([]);
   const [productsOnCartLength, setProductsOnCartLength] = useState(0);
   const { findAll } = ProductService;
   const navigate = useNavigate();
 
   useEffect(() => {
     loadData();
-    getProdutosOnCache()
+    getProdutosOnCache();
   }, []);
 
-  const getProdutosOnCache = async () => {
+  const getProdutosOnCache = () => {
     const produtos = localStorage.getItem('produtos');
     if (produtos) {
-      setProductsOnCartLength(JSON.parse(produtos).length)
-      setShowCart(true)
+      const parsedProdutos = JSON.parse(produtos);
+      setProductsOnCartLength(parsedProdutos.length);
+      setCartItems(parsedProdutos);
     }
-  }
+  };
 
   const loadData = async () => {
-    const response = await findAll();
-    console.log(response.data)
-    if (response.status === 200) {
-      setData(response.data);
-      setDataFiltred(response.data)
-      setApiError("");
-    } else {
+    try {
+      const response = await findAll();
+      console.log(response.data);
+      if (response.status === 200) {
+        setData(response.data);
+        setDataFiltered(response.data);
+        setApiError("");
+      } else {
+        setApiError("Falha ao carregar a lista de produtos.");
+      }
+    } catch (error) {
+      console.error("Erro ao carregar dados:", error);
       setApiError("Falha ao carregar a lista de produtos.");
     }
   };
 
   const addOnCart = (product: IProduct) => () => {
-    let arrayProdutosLocalStorage = []
-    const produtosFromLocalStorage = localStorage.getItem('produtos');
+    let updatedCartItems = [...cartItems];
+    const existingItem = updatedCartItems.find(item => item.produtoId === product.id);
 
-    if (produtosFromLocalStorage) {
-      arrayProdutosLocalStorage = JSON.parse(produtosFromLocalStorage);
+    if (existingItem) {
+      existingItem.quantidade += 1;
+    } else {
+      const newCartItem = {
+        produtoId: product.id,
+        produtoNome: product.name,
+        produtoValorTotal: product.price,
+        quantidade: 1
+      };
+      updatedCartItems.push(newCartItem);
     }
 
-    const produtoSelecionadoObj = {
-      produtoIndice: arrayProdutosLocalStorage.length + 1,
-      produtoId: product.id,
-      produtoNome: product.name,
-      produtoValorTotal: product.price,
-      produtoValorUnitario: product.price,
-      produtoQuantidade: 1
-    };
+    setProductsOnCartLength(updatedCartItems.length);
+    setCartItems(updatedCartItems);
+    localStorage.setItem('produtos', JSON.stringify(updatedCartItems));
+  };
 
-    arrayProdutosLocalStorage.push(produtoSelecionadoObj);
-    setProductsOnCartLength(arrayProdutosLocalStorage.length);
-    setShowCart(true)
-    return localStorage.setItem('produtos', JSON.stringify(arrayProdutosLocalStorage));
-    
-  }
+  const openModal = () => setShowModal(true);
+  const closeModal = () => setShowModal(false);
 
   const onClickOpenCart = () => {
     try {
       const produtos = localStorage.getItem("produtos");
       if (!produtos) {
-        return
+        return;
       }
-
+      setCartItems(JSON.parse(produtos));
+      openModal();
     } catch (err) {
+      console.error("Erro ao acessar produtos no carrinho:", err);
     }
   };
 
   const goToProductPage = (product: IProduct) => () => {
-    return navigate("/productpage" + product)
-  }
-
-  const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    if (event && event.target && event.target.value)
-      return setDataFiltred(dataFiltred.filter(data => data.category.name.toLowerCase().includes(event.target.value.toLowerCase()) || data.name.toLowerCase().includes(event.target.value.toLowerCase())));
-    else
-      setDataFiltred(data)
+    navigate("/productpage" + product);
   };
 
-
+  const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const inputValue = event.target.value.toLowerCase();
+    setDataFiltered(data.filter((product) =>
+      product.category.name.toLowerCase().includes(inputValue) || product.name.toLowerCase().includes(inputValue)
+    ));
+  };
 
   return (
     <>
@@ -111,25 +115,20 @@ export function ProductList() {
           }}
         />
         <CiSearch style={{ fontSize: '24px', cursor: 'pointer', color: '#555' }} />
-        {showCart && (
-          <div className="d-flex justify-content-end" role="group" aria-label="Exemplo básico">
-            <button type="button" className="btn btn-light" id="cart" >
-            <ModalExemplo />
+        <div className="d-flex justify-content-end" role="group" aria-label="Exemplo básico">
+          <button type="button" className="btn btn-light" onClick={onClickOpenCart}>
+            <BsCart2 style={{ fontSize: '30px', cursor: 'pointer', color: '#555', marginTop: '-1px' }} />
             {productsOnCartLength}
-              <BsCart2 style={{ fontSize: '30px', cursor: 'pointer', color: '#555', marginTop: '-1px' }} textAnchor="kjdnajknsdas" />
-            </button>
-          </div>
-        )}
-
-
+          </button>
+        </div>
       </form>
       <div className="product-grid">
-        {dataFiltred.length < 1 && (
-          <div className="alert alert-danger">Não há produtos para os filtros especificados</div>
+        {dataFiltered.length === 0 && (
+          <div className="alert alert-danger">Não há produtos para os filtros especificados.</div>
         )}
-        {dataFiltred.map(product => (
-          <div className="product-card" style={{ position: 'relative', paddingBottom: '50px' }}>
-            <div key={product.id} onClick={() => goToProductPage(product)}>
+        {dataFiltered.map(product => (
+          <div className="product-card" key={product.id} style={{ position: 'relative', paddingBottom: '50px' }}>
+            <div onClick={() => goToProductPage(product)}>
               <img
                 src={"https://moodle.utfpr.edu.br/pluginfile.php/1/core_admin/logocompact/300x300/1707141966/UTFPR%20-%20Identidade%20Visual%20-2.png"}
                 alt={product.name}
@@ -140,21 +139,35 @@ export function ProductList() {
                 {product.price.toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' })}
               </p>
               <p className="product-description">{product.description}</p>
-              <p></p>
             </div>
             <div className="d-flex flex-column align-items-center" style={{ position: 'absolute', bottom: '10px', width: '83%' }}>
-              <Button colorScheme='blue' size="sm" className="mb-2 mt-6">Mais Informações</Button>
-              <Button colorScheme='green' size="sm" onClick={addOnCart(product)}>Adicionar</Button>
+              <Button size="sm" className="btn btn-primary mb-2 mt-6">Mais Informações</Button>
+              <Button className='btn btn-success' size="sm" onClick={addOnCart(product)}>Adicionar</Button>
             </div>
           </div>
         ))}
         {apiError && <div className="alert alert-danger">{apiError}</div>}
       </div>
-      
-      <div className="modal-dialog modal-dialog-scrollable"  id='myModal'>
-          ...
-      </div>
+      {/* Modal de carrinho */}
+      <Modal show={showModal} onHide={closeModal}>
+        <Modal.Header closeButton>
+          <Modal.Title>Itens no Carrinho</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <ul>
+            {cartItems.map(item => (
+              <li key={item.produtoId}>
+                {item.produtoNome} - Quantidade: {item.quantidade} - Total: {item.produtoValorTotal.toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' })}
+              </li>
+            ))}
+          </ul>
+        </Modal.Body>
+        <Modal.Footer>
+          <Button variant="secondary" onClick={closeModal}>
+            Fechar
+          </Button>
+        </Modal.Footer>
+      </Modal>
     </>
   );
-};
-
+}
